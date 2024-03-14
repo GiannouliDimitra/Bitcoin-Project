b41236db76cc01803863b2d50eff96776f005733
GiannouliDimitra <giannouli.dimitra.ilianna@gmail.com>
2024-03-14 23:43:23 +0200

    complete the task

--- main.js
+++ main.js
@@ -15,14 +15,15 @@ let textOfFive = document.getElementById("resultOfFive");
 let textOfThirty = document.getElementById("resultOfThirty");
 let textOfSixty = document.getElementById("resultOfSixty");
 
- //event listener get result
- let submitBut = document.getElementById("submit");
- submitBut.addEventListener("click", getResult);
+//event listener get result
+let submitBut = document.getElementById("submit");
+submitBut.addEventListener("click", getResult);
 
- //fetch the API every single minute
- setInterval(async () => {
-	let response = await fetch(`https://api.coinbase.com/v2/prices/spot?currency=EUR`);
+
+ const fetchData = async () => {
+    let response = await fetch(`https://api.coinbase.com/v2/prices/spot?currency=EUR`);
     let data = await (response).json();
+
 //add the values in the arrays and add them to the sum__ variable
     fiveMinData.push(Number(data.data.amount));
     sumOfFive += Number(data.data.amount);
@@ -33,51 +34,45 @@ let textOfSixty = document.getElementById("resultOfSixty");
     sixtyMinData.push(Number(data.data.amount));
     sumOfSixty += Number(data.data.amount);
 
-//mean value of 5 minutes
-      
+//mean value of 5 minutes     
 if(fiveMinData.length === 5) {
     meanOfFive = Math.round(sumOfFive/5);
-    console.log ("5",meanOfFive);
     textOfFive.innerHTML = meanOfFive;
-    }
-
+    };
 if(fiveMinData.length>5) {
     sumOfFive = sumOfFive - fiveMinData[0];
     meanOfFive = Math.round(sumOfFive/5);
     textOfFive.innerHTML = meanOfFive;
-    fiveMinData.shift()
-    fiveMinData.push(Number(data.data.amount))
-    }
+    fiveMinData.shift();
+    };
   
 //mean value of 30 minutes      
 if(thirtyMinData.length === 30) {
     meanOfThirty = Math.round(sumOfThirty/30);
-    console.log ("30",meanOfThirty , thirtyMinData);
     textOfThirty.innerHTML = meanOfThirty;
-    }
-
+    };
 if(thirtyMinData.length>30) {
     sumOfThirty = sumOfThirty - thirtyMinData[0];
     meanOfThirty = Math.round(sumOfThirty/30);
     textOfThirty.innerHTML = meanOfThirty;
-    thirtyMinData.shift()
-    thirtyMinData.push(Number(data.data.amount))
-    }
+    thirtyMinData.shift();
+    };
 
+//mean value of 60 minutes 
 if(sixtyMinData.length === 60){
 meanOfSixty = Math.round(sumOfSixty/60);
-console.log ("60",meanOfSixty);
 textOfSixty.innerHTML = meanOfSixty;
-}
+};
 if(sixtyMinData.length>60) {
     sumOfSixty = sumOfSixty - sixtyMinData[0];
     meanOfSixty = Math.round(sumOfSixty/60);
     textOfSixty.innerHTML = meanOfSixty;
     sixtyMinData.shift();
-    sixtyMinData.push(Number(data.data.amount))
-}
+};
 
+//plot the results on a chart
 document.getElementById('chart').innerHTML = '';
+
 var options = {
     chart: {
       type: 'line',
@@ -101,36 +96,35 @@ var options = {
       decimalsInFloat: false,
     },
     title: {
-text: "Bitcoin values",
+    text: "Bitcoin values",
     align: 'center',
     margin: 10,
     offsetX: 0,
     offsetY: 30,
     floating: false,
     }
-  }
+  };
   
   var chart = new ApexCharts(document.querySelector("#chart"), options);
   chart.render(); 
  
-}, 2000);
+};
+fetchData();
+setInterval (fetchData,1000);
 
-
-
-    async function getResult (event) {
+async function getResult (event) {
     try {
         let currency = document.getElementById("coinType").value;
-        event.preventDefault()
+        event.preventDefault();
         let response = await fetch(
             `https://api.coinbase.com/v2/prices/spot?currency=${currency}`
         );
         let data = await (response).json();
-        document.getElementById("textField").value = `${data.data.amount} ${currency}`;
-        
+        document.getElementById("textField").value = `${data.data.amount} ${currency}`;    
     } catch (error) {
     console.log(error) 
     }
-}
+};
 
 
 
